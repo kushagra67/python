f843a561b126090721a2e74c13d1e33fd64916a4
kushagra67 <kushagra.singhal@btech.christuniversity.in>
2025-01-13 01:09:22 +0530

    Create 2.py
    phyton basics

---
+++ 2.py
@@ -0,0 +1,25 @@
+x = 20
+y = "kushagra"
+
+print(x)
+print(y)
+
+print(type(x))
+
+
+x,y,z = "1","2","3"
+print(type(x))
+ 
+fruits = ["apple","orange","mango"]
+print (fruits)
+
+x,y,z = fruits
+
+print(x)    
+print(y)
+print(z)
+
+# slicing 
+
+p = "kushagra"
+print(p[2:5])
