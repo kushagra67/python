f63d01c5e96a44a71ec14ab797a699388ba3a912
kushagra67 <kushagra.singhal@btech.christuniversity.in>
2025-01-13 16:47:50 +0530

    Create 4.py
    Tuples

---
+++ 4.py
@@ -0,0 +1,16 @@
+# tuples 
+
+list = (1, 2, 3)
+print(list)
+
+# type of tuple 
+
+thistuple = ("apple" ,)
+
+print(type(thistuple))
+
+#not a tuple
+thistuple = ("apple")
+
+print(type(thistuple))
+
